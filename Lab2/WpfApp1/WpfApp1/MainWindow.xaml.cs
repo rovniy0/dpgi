f54d7626c8ea97865fbe57a5af95fca060f2089f
Grisha Rovniy <rovniygrishaaa@gmail.com>
2025-03-09 17:33:29 +0200

    add command binding, update interface

--- Lab2/WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ Lab2/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,17 +13,68 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+
 
 namespace WpfApp1
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            var openBinding = new CommandBinding(ApplicationCommands.Open,
+            Execute_Open, CanExecute_Open);
+            var closeBinding = new CommandBinding(ApplicationCommands.Close,
+            Execute_Close, CanExecute_Close);
+            var saveBinding = new CommandBinding(ApplicationCommands.Save,
+            Execute_Save, CanExecute_Save);
+            CommandBindings.Add(openBinding);
+            CommandBindings.Add(closeBinding);
+            CommandBindings.Add(saveBinding);
+        }
+        private void CanExecute_Save(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = MainTextBox.Text.Trim().Length > 0;
+        }
+        private void Execute_Save(object sender, ExecutedRoutedEventArgs e)
+        {
+            File.WriteAllText("Labka.txt", MainTextBox.Text);
+            MessageBox.Show("This file was saved ^-^ ");
+        }
+
+        private void CanExecute_Open(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = MainTextBox.Text.Trim().Length == 0;
+        }
+        private void Execute_Open(object sender, ExecutedRoutedEventArgs e)
+        {
+            const string filter = "Text files (*.txt)|*.txt;|All files (*.*)|*.*";
+
+            var openDialog = new OpenFileDialog()
+            {
+                InitialDirectory = AppDomain.CurrentDomain.BaseDirectory,
+                Filter = filter
+            };
+            if (openDialog.ShowDialog() == true)
+            {
+                MainTextBox.Text = File.ReadAllText(openDialog.FileName);
+            }
+        }
+
+        private void CanExecute_Close(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = MainTextBox.Text.Length > 0;
+        }
+        private void Execute_Close(object sender, ExecutedRoutedEventArgs e)
+        {
+            MainTextBox.Text = "";
         }
     }
+
 }
